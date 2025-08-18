70f57df884934d783d93b9c318e2d9829ae4deb0
Anik-web-code <alianik11star@gmail.com>
2025-08-18 07:40:37 +0600

    Updated Hero Section

--- src/Components/Homepage/Hero.jsx
+++ src/Components/Homepage/Hero.jsx
@@ -3,18 +3,18 @@ import { NavLink } from "react-router";
 
 const Hero = () => {
   return (
-    <div className="mb-20">
+    <div className="mb-20 mt-10">
       <div
         className="bg-cover rounded-[16px] bg-center mx-auto h-[500px] w-[95%] lg:h-[600px] md:w-[90%] lg:w-[80%] md:h-screen flex-shrink-0 p-7"
         style={{
           backgroundImage:
-            'url("https://i.postimg.cc/Nfqz1PG0/istockphoto-1328426310-612x612-1.jpg")',
+            'url("https://i.postimg.cc/zv2M2KvB/hands-of-the-giver-and-the-recipient-to-donate-blood-blood-donation-concept-heart-medical-sign-give.jpg")',
         }}
       >
         <h1 className="text-[40px] sm:text-[56px] md:text-[80px] text-[#ff0033] font-medium mb-8">
           Be the Lifeline – Donate Blood, Save Lives
         </h1>
-        <p className="text-[16px] md:text-[32px] text-black font-medium mb-10">
+        <p className="text-[16px] md:text-[32px] text-[#FFFFFF] font-medium mb-10">
           Every drop counts. Join our mission to bring hope and health to those
           in need. Your simple act of donating blood can give someone another
           chance at life. Sign up today and be the hero someone is waiting for
